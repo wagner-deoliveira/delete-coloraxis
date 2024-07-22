edf83acd023ed6634d9e771e45b5b02fad7a0ba0
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2024-07-22 15:17:15 +0200

    Delete also DDaCT

--- delete_coloraxis.py
+++ delete_coloraxis.py
@@ -1,19 +1,21 @@
 from os import chdir
 
-#First, enter the path to file QA_VISUALS.txt
-#Example path
+# First, enter the path to file QA_VISUALS.txt
+# Example path
 path = 'C:/DDaCT/SSC-1779/Comparison1'
 chdir(path)
 
-with open("QA_VISUALS.txt", "r") as qa_file:
-    lines = qa_file.readlines()
-   
-#Path to where you want to save the modified file, without coloraxis information
-#Example path
-save_path = 'C:/Users/Desktop'
-chdir(save_path)
 
-with open("sample.txt", "w") as new_file:
-    for line in lines:
-        if not "coloraxis" in line:
-            new_file.write(line)
+def read_delete_coloraxis(path) -> None:
+    chdir(path)
+    with open("QA_VISUALS.txt", "r") as qa_file:
+        lines = qa_file.readlines()
+
+    with open("QA_VISUALS.txt", "w") as new_file:
+        for line in lines:
+            if not "coloraxis" or not "DDaCT" in line:
+                new_file.write(line)
+
+
+if __name__ == '__main__':
+    read_delete_coloraxis(path)
